99c046baa633f22e522c72a2021ca80479efddb2
ManuelCufre <manuelcufree@.com>
2025-08-15 17:00:41 -0300

    Codigo con mis datos en el Incio

--- frontend/src/Inicio.jsx
+++ frontend/src/Inicio.jsx
@@ -2,22 +2,22 @@ import { FaGithub } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import { IoMdDownload } from "react-icons/io";
 import { FiGithub } from "react-icons/fi";
-import { FaLinkedinIn } from "react-icons/fa";
 import Imagen from './img/carbon.png'
 import { FaCode } from "react-icons/fa6";
 import { Button } from "@chakra-ui/react";
 import { PiBracketsCurly } from "react-icons/pi";
 import useMode from "./hooks/useMode";
+import { FaLinkedinIn } from "react-icons/fa";
 export default function Inicio() {
   const { modo } = useMode();
 
   return (
     <div
-      className="w-[65%] min-h-[100vh] flex justify-center items-center fira-code"
+      className="w-[65%] min-h-[90vh] flex justify-center items-center fira-code"
       id="inicio"
     >
       <div className="w-full min-h-[50vh] flex gap-8 justify-center items-center rounded-2xl  bg-[#DBDBDB] text-black dark:bg-[#1E1E1F] dark:text-white">
-        <div className="flex  gap-16 items-center nata-sans">
+        <div className="flex relative top-16 gap-16 items-center nata-sans">
           <div className="w-[50%]">
             <div className="flex flex-col ">
               <h2 className="bold text-white">Hola soy Manuel Cufré</h2>
@@ -35,9 +35,12 @@ export default function Inicio() {
             </p>
 
             {/* github */}
-            <div className="flex gap-4">
-              <div className="w-10 h-10 !border-1 !border-gray-400 rounded-full flex items-center justify-center">
-                <FiGithub size={20} />
+            <div className="flex gap-2">
+              <div className="w-10 h-10 !border-1 !border-teal-500 rounded-full flex items-center justify-center">
+                <FiGithub size={20} className="text-teal-500"/>
+              </div>
+              <div className="w-10 h-10 !border-1 !border-teal-500 rounded-full flex items-center justify-center">
+                <FaLinkedinIn size={20} className="text-teal-500"/>
               </div>
             </div>
 
@@ -45,7 +48,7 @@ export default function Inicio() {
             <div className=" flex gap-4 ">
               <a href="">
                 <Button colorPalette="teal" variant="solid">
-                  <IoMdDownload size={20} />
+                  <IoMdDownload size={24} />
                   Descargar CV
                 </Button>
               </a>
@@ -62,7 +65,7 @@ export default function Inicio() {
               </a>
             </div>
           </div>
-          <div className="w-[40%] rounded-xl "> 
+          <div className="w-[50%] rounded-2xl !border-1 !border-gray-600"> 
            <img src={Imagen} alt="" className="rounded-2xl"/>
           </div>
         </div>
